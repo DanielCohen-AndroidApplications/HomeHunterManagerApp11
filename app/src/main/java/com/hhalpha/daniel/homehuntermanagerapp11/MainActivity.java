a662ea0e09b1a7b6518173d56493670c93bc0553
dcofosho <dlcohen224@gmail.com>
2016-06-20 18:29:32 -0400

    Retrieved and displayed images from s3 in imageView

--- app/src/main/java/com/hhalpha/daniel/homehuntermanagerapp11/MainActivity.java
+++ app/src/main/java/com/hhalpha/daniel/homehuntermanagerapp11/MainActivity.java
@@ -2,6 +2,9 @@ package com.hhalpha.daniel.homehuntermanagerapp11;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.mobileconnectors.cognito.CognitoSyncManager;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
@@ -23,7 +27,16 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.iterable.S3Objects;
 import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.util.IOUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
 
 /**
  * Created by Daniel on 5/30/2016.
@@ -38,11 +51,21 @@ public class MainActivity extends Activity {
     TransferUtility transferUtility;
     DynamoDBMapper mapper;
     AmazonDynamoDB dynamoDB;
+    ArrayList<String> addresses;
+    File pic1;
+    Bitmap bitmapTest;
+    FileOutputStream fos;
+    String keyTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try {
+            Log.v("_danoncreate",getApplicationContext().getCacheDir().listFiles().toString());
+
+
+            addresses=new ArrayList<String>();
+
             listBtn = (Button) findViewById(R.id.listBtn);
             createBtn = (Button) findViewById(R.id.createBtn);
             imageViewTest = (ImageView) findViewById(R.id.imageViewTest);
@@ -79,10 +102,11 @@ public class MainActivity extends Activity {
         Intent i= new Intent(MainActivity.this, CreateActivity.class);
         startActivity(i);
     }
-    public class retrieveTask extends AsyncTask<String,Integer,String> {
+
+    public class retrieveTask extends AsyncTask<String,Integer,ArrayList<String>> {
 
         @Override
-        protected String doInBackground(String... params) {
+        protected ArrayList<String> doInBackground(String... params) {
 
             try {
                 s3 = new AmazonS3Client(credentialsProvider);
@@ -91,12 +115,25 @@ public class MainActivity extends Activity {
                 s3.setRegion(Region.getRegion(Regions.US_EAST_1));
                 transferUtility = new TransferUtility(s3, getApplicationContext());
                 for (S3ObjectSummary summary : S3Objects.inBucket(s3, "hhproperties")) {
+                    addresses.add(summary.getKey());
                     Log.v("_dan", summary.getKey());
+                    keyTest=summary.getKey();
                 }
-            }catch(Exception e){
-                Log.v("_dan", e.getMessage());
+                S3ObjectInputStream content = s3.getObject("hhproperties", keyTest).getObjectContent();
+                byte[] bytes = IOUtils.toByteArray(content);
+                bitmapTest = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                Log.v("_dan", addresses.toString());
+
+            } catch (Exception e) {
+                Log.v("_dandoib", e.getMessage());
             }
-            return null;
+            return addresses;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> strings) {
+            textViewTest.setText(strings.toString());
+            imageViewTest.setImageBitmap(bitmapTest);
         }
     }
 }
